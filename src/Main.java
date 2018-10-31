c22002bf370d2a6a823826c1411bc4f8783769e5
Jacky Boy <jts4@st-andrews.ac.uk>
2018-10-31 01:48:49 +0000

    Nearly finished with basic game, need to polish it up but looking good so far

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,6 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-
-        World world = World.EASY1;
-        Part1 part1 = new Part1(world);
-//        World.EASY1.printWorld();
-        playGame(world);
-    }
-
     public static int[] getCoords(String in) {
         String[] moveSpl = in.split(" ");
         int[] coords = new int[2];
@@ -18,42 +10,67 @@ public class Main {
         return coords;
     }
 
-    public static World uncoverCells(World world, int[] coords) {
-        int startY = coords[0] - 1, endY = coords[0] + 1;
-        if (startY < 0)
-            startY = 0;
-        if (endY >= world.length)
-            endY = world.length - 1;
-        int startX = coords[1] - 1, endX = coords[1] + 1;
-        if (startX < 0)
-            startX = 0;
-        if (endX >= world.length)
-            endX = world.length - 1;
-        for (int i = startY; i <= endY; i++) {
-            for (int j = startX; j <= endX; j++) {
-                if (world.map[i][j] == "0") {
-                    world.map[i][j] = "X";
-                    uncoverCells(world, new int[]{i, j});
+    public static int uncoverCells(World world, int[] coords) {
+        int live = 0; // either adds a life if gold is found, subtracts if dagger is found, and stays at 0 if anything else is found at current coords choice
+        if (world.map[coords[0]][coords[1]] == "0") {
+            int startY = coords[0] - 1, endY = coords[0] + 1;
+            if (startY < 0)
+                startY = 0;
+            if (endY >= world.length)
+                endY = world.length - 1;
+            int startX = coords[1] - 1, endX = coords[1] + 1;
+            if (startX < 0)
+                startX = 0;
+            if (endX >= world.length)
+                endX = world.length - 1;
+            for (int i = startY; i <= endY; i++) {
+                for (int j = startX; j <= endX; j++) {
+                    if (world.map[i][j] == "0") {
+                        world.displMap[i][j] = world.map[i][j];
+//                    uncoverCells(world, new int[]{i, j}); // use for uncovering all adjacent cells with 0 for minesweeper
+                    }
+                    System.out.print(" " +world.displMap[i][j]);
                 }
-                System.out.print(" " +world.map[i][j]);
+                System.out.println();
             }
             System.out.println();
         }
-        System.out.println();
-        return world;
+        if (world.map[coords[0]][coords[1]] == "d")
+            live = -1;
+        if (world.map[coords[0]][coords[1]] == "g")
+            live = 1;
+        world.displMap[coords[0]][coords[1]] = world.map[coords[0]][coords[1]];
+        return live;
     }
 
-    public static void playGame(World world) {
-        Scanner userIn = new Scanner(System.in);
+    public static boolean gameIsOver(int lives) {
         boolean gameOver = false;
-        while (!gameOver) {
+        if (lives == 0) {
+            gameOver = true;
+            System.out.println("You died!");
+        }
+        return gameOver;
+    }
+
+    public static void playGame(World world, int lives) {
+        Scanner userIn = new Scanner(System.in);
+        while (!gameIsOver(lives)) {
             world.printWorld();
             System.out.print("Input coords: ");
             String move = userIn.nextLine();
             System.out.println();
             int[] coords = getCoords(move);
-            uncoverCells(world, coords);
+            lives += uncoverCells(world, coords);
         }
         userIn.close();
+    }
+
+    public static void main(String[] args) {
+
+        int lives = 1;
+        World world = World.EASY1;
+        Part1 part1 = new Part1(world);
+//        World.EASY1.printWorld();
+        playGame(world, lives);
     }
 }
